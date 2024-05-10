b2532717c056e933c8a1927094430ea77323ffc3
EraldoCarlosFh <eraldo1904@gmail.com>
2024-05-10 02:50:33 -0300

    chore: add next auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { AuthOptions, } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prismaClient } from "@/lib/prisma";
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   prisma = global.cachedPrisma;
 }
 
-export const db = prisma;
+export const prismaClient = prisma;
 
 
 
