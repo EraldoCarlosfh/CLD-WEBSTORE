eabab13bd1f4ff1b230311965a6f2cb3a4a69120
EraldoCarlosFh <eraldo1904@gmail.com>
2024-05-12 00:04:56 -0300

    feat: add CatalogPage

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,28 @@
-const CatalogPage = () => {
-    return ( 
-        <h1>Catálogo</h1>
-     );
-}
- 
-export default CatalogPage
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+import { prismaClient } from "@/lib/prisma";
+
+const CatalogPage = async () => {
+  const categorys = await prismaClient.categorys.findMany({});
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.372rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 flex-wrap gap-8">
+        {categorys.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CatalogPage;
