7861699ba104fb831c225cd6437d21ec299ca063
EraldoCarlosFh <eraldo1904@gmail.com>
2024-05-12 01:43:19 -0300

    fix: adjustment made to include products when searching category

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,17 +1,21 @@
 import { Badge } from "@/components/ui/badge";
 import ProductItem from "@/components/ui/product-item";
 import { CATEGORY_ICON } from "@/constants/category-icon";
-import { convertNameSlug } from "@/constants/convert-name-slug";
 import { prismaClient } from "@/lib/prisma";
 
 const CategoryProducts = async ({ params }: any) => {
-  const products = await prismaClient.products.findMany({
+  const category = await prismaClient.categorys.findFirst({
     where: {
-      category: {
-        slug: params.slug,
-      },
+      slug: params.slug,
     },
-  }); 
+    include: {
+      products: true,
+    },
+  });
+
+  if (!category) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col gap-8 p-5">
@@ -20,11 +24,11 @@ const CategoryProducts = async ({ params }: any) => {
         variant="outline"
       >
         {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
-        {convertNameSlug(params.slug)}
+        {category.name}
       </Badge>
 
       <div className="grid grid-cols-2 gap-8">
-        {products.map((product) => (
+        {category.products.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
       </div>
