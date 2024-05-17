f93c73e3c917ab5720b96e4df7c1495033ffe48f
EraldoCarlosFh <eraldo1904@gmail.com>
2024-05-17 02:23:23 -0300

    feat: adjust cod

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
     images: {
         domains: ["xarwas4csfe8g80s.public.blob.vercel-storage.com"]
+    },
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
     }
 };
 
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,23 +4,3 @@ import { authOptions } from "@/lib/auth";
 const handlers = NextAuth(authOptions);
 
 export { handlers as GET, handlers as POST };
-
-// import NextAuth, { AuthOptions, } from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import GithubProvider from "next-auth/providers/github";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { prismaClient } from "@/lib/prisma";
-
-// export const { handlers, auth, signIn, signOut } = NextAuth({
-//   adapter: PrismaAdapter(prismaClient),
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//   ],
-// });
